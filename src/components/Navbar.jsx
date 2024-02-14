7f658f5a3b220bd1c0e5150b89606309032afdd3
Zachariah Prajeeyachat <titan.zp4@gmail.com>
2024-02-15 00:06:52 +0700

    Add hyperlink connecting to my web

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,9 @@
 const Navbar = () => {
     return (
         <nav>
-            <h1>Zach&apos;s Tomato🍅</h1>
+            <a href="https://exzachly.notion.site" target="_blank" rel="noreferrer">
+                <h1>Zach&apos;s Tomato🍅</h1>
+            </a>
             <p>New minimalism Pomodoro timer</p>
         </nav>
     )
