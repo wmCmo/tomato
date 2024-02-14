39ea26afa4c36d82b35aa00a38b3836d2045ea18
Zachariah Prajeeyachat <titan.zp4@gmail.com>
2024-02-14 18:34:29 +0700

    Use Phosphoricons for React library

--- src/components/ControlButton.jsx
+++ src/components/ControlButton.jsx
@@ -1,8 +1,28 @@
 /* eslint-disable react/prop-types */
+import { IconContext, ArrowCounterClockwise, ArrowArcLeft, ArrowArcRight, Pause, Play } from "@phosphor-icons/react";
+
 const ControlButton = (props) => {
+    let render;
+    if (props.file === 'reset') {
+        render = <ArrowCounterClockwise />;
+    } else if (props.file === 'backward') {
+        render = <ArrowArcLeft />;
+    } else if (props.file === 'pause') {
+        render = <Pause />;
+    } else if (props.file === 'play') {
+        render = <Play />;
+    } else if (props.file === 'forward') {
+        render = <ArrowArcRight />;
+    }
     return (
         <button onClick={props.btnFunc}>
-            <img src={`./src/assets/${props.file}-icon.svg`} alt="props.file" />
+            <IconContext.Provider value={{
+                color: 'white',
+                size: 25,
+                weight: 'fill'
+            }}>
+                {render}
+            </IconContext.Provider>
         </button>
     )
 }
