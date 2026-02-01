937bb4c207433ab12749246b4fdb2a72867539be
Zachariah Prajeeyachat <titan.zp4@gmail.com>
2026-02-01 18:19:19 +0900

    feat: update dependencies and add PWA support
    - Added `@supabase/supabase-js` dependency for enhanced functionality.
    - Updated `eslint` and `eslint-plugin-react-hooks` to their latest versions.
    - Introduced `vite-plugin-pwa` for Progressive Web App capabilities.
    - Added new icons and manifest for PWA support.
    - Included new logo and apple touch icon for branding.
    - Updated `package.json` with overrides for browserslist.
    - Modified `vite.config.js` to configure PWA settings and asset inclusion.

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,28 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  plugins: [react()],
+  plugins: [react(), VitePWA({
+    registerType: 'autoUpdate', injectRegister: "auto", includeAssets: [
+      'favicon.ico',
+      'logo.svg',
+      'pwa-64x64.png',
+      'pwa-192x192.png',
+      'pwa-512x512.png',
+      'maskable-icon-512x512.png',
+      'apple-touch-icon-180x180.png',
+      'og.png',
+      'manifest.webmanifest'
+    ],
+    devOptions: {
+      enabled: true
+    },
+    workbox: {
+      navigateFallbackDenylist: [/^\/tomato\/assets\//]
+    }
+  })],
   // Use / in dev for convenience, but /tomato/ in production (GitHub Pages project site)
   base: mode === 'production' ? '/tomato/' : '/',
-}))
+}));
