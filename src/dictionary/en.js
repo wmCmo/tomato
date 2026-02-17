245443785ba58195e98fa94bb03b7e83fc51f206
Zachariah Prajeeyachat <titan.zp4@gmail.com>
2026-02-17 19:49:43 +0900

    feat: setup initial dictionary based translation infrastructure

--- src/dictionary/en.js
+++ src/dictionary/en.js
@@ -1,15 +1,42 @@
 const en = {
-    nav: {
-        header: "Zach's Tomato",
-        desc: "Your minimal Pomodoro timer"
+    components: {
+        return2Home: 'Home'
     },
-    choices: ['Pomodoro', 'Short Break', 'Long Break'],
-    messages: {
-        start: "Click Start and let's get things done!",
-        work: "Time to Work!",
-        rest: "Time to Rest!"
+    home: {
+        nav: {
+            header: "Zach's Tomato",
+            desc: "Your minimal Pomodoro timer"
+        },
+        choices: ['Pomodoro', 'Short Break', 'Long Break'],
+        messages: {
+            start: "Click Start and let's get things done!",
+            work: "Time to Work!",
+            rest: "Time to Rest!"
+        },
+        session: "Session",
     },
-    session: "Session"
+    profile: {
+        welcome: 'Welcome back,',
+        status: 'I want to tell everyone that...',
+        highScore: 'High Score',
+        emptySession: 'Empty session',
+        thisWeek: 'This week',
+        viewMore: 'View More',
+        days: [
+            'Sun',
+            'Mon',
+            'Tue',
+            'Wed',
+            'Thu',
+            'Fri',
+            'Sat',
+        ]
+    },
+    record: {
+        header: 'Your Record',
+        return: 'Back to Profile',
+        empty: ['Your sessions are currently ', 'empty.', " Let's go back to homepage and lock in!~"]
+    },
 };
 
 export default en;
