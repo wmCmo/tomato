109776401f30194e07e1d74ab2d60a205e69a33d
Zachariah Prajeeyachat <titan.zp4@gmail.com>
2026-03-12 23:12:36 +0900

    fix: correct race states for redirect in /settings

--- types/AuthContext.ts
+++ types/AuthContext.ts
@@ -2,6 +2,6 @@ import type { Session, User } from "@supabase/supabase-js";
 
 export default interface AuthContextType {
     session: Session | null;
-    user: User | null;
+    user: User | null | undefined;
     loading: boolean;
 }
